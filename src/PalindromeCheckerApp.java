bd55c03c383daa0f5291102360783a2daa834fc8
kevin-bennette <kevinbennete006@gmail.com>
2026-02-26 09:06:48 +0530

    initial

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -6,7 +6,6 @@ public class PalindromeCheckerApp {
         System.out.print("Enter a long string for performance test: ");
         String input = scanner.nextLine();
 
-        // Test String Reverse [cite: 105]
         long start = System.nanoTime();
         isPalindromeReverse(input);
         long end = System.nanoTime();
