951a0db37f6ea58d7a3a0622dd1a42343d174abb
kevin-bennette <kevinbennete006@gmail.com>
2026-02-26 09:04:41 +0530

    initial

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,29 +1,35 @@
 import java.util.Scanner;
 
-class PalindromeService {
-    public boolean checkPalindrome(String input) {
-        if (input == null) return false;
-        int i = 0, j = input.length() - 1;
-        while (i < j) {
-            if (input.charAt(i++) != input.charAt(j--)) return false;
-        }
-        return true;
-    }
-}
-
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        PalindromeService service = new PalindromeService();
-
-        System.out.print("Enter string: ");
+        System.out.print("Enter a long string for performance test: ");
         String input = scanner.nextLine();
 
-        if (service.checkPalindrome(input)) {
-            System.out.println(input + " is a palindrome.");
-        } else {
-            System.out.println(input + " is not a palindrome.");
-        }
+        // Test String Reverse [cite: 105]
+        long start = System.nanoTime();
+        isPalindromeReverse(input);
+        long end = System.nanoTime();
+        System.out.println("String Reverse Time: " + (end - start) + " ns");
+
+        // Test Two-Pointer
+        start = System.nanoTime();
+        isPalindromeTwoPointer(input);
+        end = System.nanoTime();
+        System.out.println("Two-Pointer Time: " + (end - start) + " ns");
+
         scanner.close();
+    }
+
+    public static boolean isPalindromeReverse(String s) {
+        return new StringBuilder(s).reverse().toString().equalsIgnoreCase(s);
+    }
+
+    public static boolean isPalindromeTwoPointer(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i++) != s.charAt(j--)) return false;
+        }
+        return true;
     }
 }
