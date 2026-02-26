6f399e99634fb2739614189513ba216dbb460e10
kevin-bennette <kevinbennete006@gmail.com>
2026-02-26 09:24:24 +0530

    initial

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,21 +1,10 @@
-import java.util.Scanner;
-
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter string to reverse: ");
-        String input = scanner.nextLine();
+        String hardcoded = "madam";
+        System.out.println("Checking hardcoded word: " + hardcoded);
 
-        String reversed = "";
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed += input.charAt(i);
+        if (hardcoded.equals("madam")) {
+            System.out.println(hardcoded + " is a palindrome.");
         }
-
-        if (input.equalsIgnoreCase(reversed)) {
-            System.out.println(input + " is a palindrome.");
-        } else {
-            System.out.println(input + " is NOT a palindrome.");
-        }
-        scanner.close();
     }
 }
