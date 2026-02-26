3e2ae1f195e6194b9d8c87da28ab54725488623a
Kevin Bennette <kevinbennete006@gmail.com>
2026-02-26 09:21:43 +0530

    Name Debug

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Stack;
 
-public class Main {
+public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter string: ");
