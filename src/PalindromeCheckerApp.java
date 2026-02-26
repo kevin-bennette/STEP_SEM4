fb804cfaa11d6559abf32c2cdfa7aa1ffbe287cc
kevin-bennette <kevinbennete006@gmail.com>
2026-02-26 09:01:46 +0530

    initial

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,19 +1,29 @@
 import java.util.Scanner;
 
+class PalindromeService {
+    public boolean checkPalindrome(String input) {
+        if (input == null) return false;
+        int i = 0, j = input.length() - 1;
+        while (i < j) {
+            if (input.charAt(i++) != input.charAt(j--)) return false;
+        }
+        return true;
+    }
+}
+
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        PalindromeService service = new PalindromeService(); // [cite: 69]
+
         System.out.print("Enter string: ");
         String input = scanner.nextLine();
 
-        String cleanInput = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-
-        String reversed = new StringBuilder(cleanInput).reverse().toString();
-        boolean isPalindrome = cleanInput.equals(reversed);
-
-        System.out.println("Original: " + input);
-        System.out.println("Normalized: " + cleanInput);
-        System.out.println("Result: " + (isPalindrome ? "Palindrome" : "Not Palindrome"));
+        if (service.checkPalindrome(input)) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
         scanner.close();
     }
 }
