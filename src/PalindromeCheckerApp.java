124c8eba7d0ad238bd861c222125db659b3a49f1
kevin-bennette <kevinbennete006@gmail.com>
2026-02-26 09:25:28 +0530

    initial

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,10 +1,9 @@
-public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        String hardcoded = "madam";
-        System.out.println("Checking hardcoded word: " + hardcoded);
+import java.util.Scanner;
 
-        if (hardcoded.equals("madam")) {
-            System.out.println(hardcoded + " is a palindrome.");
-        }
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Palindrome Checker App");
+        System.out.println("Version: 1.0");
+        System.out.println("Status: System Initialized.");
     }
 }
